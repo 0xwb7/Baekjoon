30d2f669b28009c7b8235fb9f7a7bc377deb70ee
이우빈 <woobin200111@gmail.com>
2025-11-04 23:30:22 +0900

    [Bronze V] Title: 꼬마 정민, Time: 172 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Long num1 = scanner.nextLong();
+        Long num2 = scanner.nextLong();
+        Long num3 = scanner.nextLong();
+
+        System.out.println(num1 + num2 + num3);
+    }
+}
