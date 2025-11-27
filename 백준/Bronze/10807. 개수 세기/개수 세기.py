ef7d65ac2a53da1e6140f6fa23cd27595df8ba4f
이우빈 <woobin200111@gmail.com>
2025-11-27 15:17:11 +0900

    [Bronze V] Title: 개수 세기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,14 +1,10 @@
 n = int(input())
-str = input()
-nl = []
-for i in range(n):
-    nl.append(int(str.split()[i]))
-
-t = int(input())
+nl = list(map(int, input().split()))
 cnt = 0
+t = int(input())
 
 for i in nl:
     if i == t:
         cnt += 1
-
+        
 print(cnt)
