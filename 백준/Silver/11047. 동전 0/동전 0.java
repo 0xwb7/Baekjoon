f35b352bdb457ea03047f2c6a6c675c09b5eac1e
이우빈 <woobin200111@gmail.com>
2026-04-03 17:56:05 +0900

    [Silver IV] Title: 동전 0, Time: 112 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int cnt = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (k >= arr[i]) {
+                int tmp = k / arr[i]; // k=4, (1, 3) 있을 때, tmp = 4 / 3 = 1  / 1
+                cnt += tmp; // cnt = 1
+                k = k - arr[i] * tmp; // 4 = 4 - 3 * 1 = 1
+
+                if (k == 0) {
+                    break;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
