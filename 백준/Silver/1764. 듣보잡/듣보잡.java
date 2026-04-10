03da826009e4d08ac175a024468cecfccb57cbbd
이우빈 <woobin200111@gmail.com>
2026-04-10 11:10:57 +0900

    [Silver IV] Title: 듣보잡, Time: 264 ms, Memory: 27176 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Map<String, Integer> map1 = new HashMap<>();
+        Map<String, Integer> map2 = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            map1.put(br.readLine(), i);
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < m; i++) {
+            String input = br.readLine();
+
+            if (map1.containsKey(input)) {
+                cnt++;
+                map2.put(input, i);
+            }
+        }
+
+        sb.append(cnt).append("\n");
+
+        List<String> list = new ArrayList<>(map2.keySet());
+        Collections.sort(list);
+
+        for (String key : list) {
+            sb.append(key).append("\n");
+        }
+
+        System.out.println(sb);
+
+        br.close();
+    }
+}
