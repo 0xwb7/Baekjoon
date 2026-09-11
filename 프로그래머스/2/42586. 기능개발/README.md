a548efb48dc6dc73f3f9546c5f65c1c460774e65
이우빈 <woobin200111@gmail.com>
2026-09-11 14:23:56 +0900

    [level 2] Title: 기능개발, Time: 0.21 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586#) 
 
 ### 성능 요약
 
-메모리: 82.7 MB, 시간: 0.09 ms
+메모리: 73.4 MB, 시간: 0.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 09월 10일 14:42:41
+2026년 09월 11일 14:23:53
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,57 +1,47 @@
 import java.util.ArrayList;
 
 class Solution {
-    public static int[] solution(int[] p, int[] s) {
+    static int[] solution(int[] p, int[] s) {
         int len = p.length;
-        ArrayList<Integer> arr = new ArrayList<>();
+        int[] duration = getDuration(p, s, len);
+        ArrayList<Integer> deployCounts = new ArrayList<>();
 
-        int idx = 0;
-        int total = 0;
-        while (true) {
-            int cnt = 0;
-            toComplete(p, s, len, idx);
-            for (int i = idx; i < len; i++) {
-                if (p[i] >= 100) {
-                    cnt++;
+        int cnt = 1;
+        int deployDay = duration[0];
 
-                    if (i == len - 1) {
-                        total += cnt;
-                        arr.add(cnt);
-                    }
-                } else {
-                    arr.add(cnt);
-                    total += cnt;
-                    idx = i;
-                    cnt = 0;
-                    break;
-                }
-
-            }
-
-            if (total == len) {
-                break;
+        for (int i = 1; i < len; i++) {
+            if (deployDay >= duration[i]) {
+                cnt++;
+            } else {
+                deployCounts.add(cnt);
+                cnt = 1;
+                deployDay = duration[i];
             }
         }
 
-        int[] retArr = new int[arr.size()];
+        deployCounts.add(cnt);
 
-        for (int i = 0; i < arr.size(); i++) {
-            retArr[i] = arr.get(i);
+        int[] retArr = new int[deployCounts.size()];
+
+        for (int i = 0; i < deployCounts.size(); i++) {
+            retArr[i] = deployCounts.get(i);
         }
 
+        System.out.println(deployCounts);
         return retArr;
     }
 
-    public static void toComplete(int[] p, int[] s, int len, int idx) {
-        while (true) {
-            if (p[idx] >= 100) {
-                break;
-            }
+    public static int[] getDuration(int[] p, int[] s, int len) {
+        int[] arr = new int[len];
 
-            for (int i = idx; i < len; i++) {
-                p[i] += s[i];
+        for (int i = 0; i < len; i++) {
+            if ((100 - p[i]) % s[i] != 0) {
+                arr[i] = (100 - p[i]) / s[i] + 1;
+            } else {
+                arr[i] = (100 - p[i]) / s[i];
             }
-
         }
+
+        return arr;
     }
 }
